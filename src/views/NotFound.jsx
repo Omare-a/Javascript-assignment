a2552894dbd567fd4796778b99dd5dbd51239188
Ömer <omareany99@hotmail.com>
2023-11-20 16:01:40 +0100

    404change

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Footer from '../componets/Footer'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
     <>
-    <div>NotFound</div>
+    <div className='notfound'>
+      <div className="container">
+      <div className='fourofour'>
+        404NotFound
+      </div>
+      <Link to="/"><button className="btn-black">Go Back Home<i className="fa-regular fa-arrow-up-right"></i></button></Link>
+      </div>
+    </div>
     <Footer></Footer>
     </>
   )
